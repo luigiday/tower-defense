590b1724ea8f22b43eef204c18f7d15dcb427445
L0uting <alfredparaire@gmail.com>
2025-12-02 23:03:36 +0100

    jsp jai mis un chemin

--- game.py
+++ game.py
@@ -1,27 +1,51 @@
 from random import randint
 import pygame
 from time import sleep
-
+path=[]
+monstres=[]
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    frozen = False
+    running = True
+    
+    clock.tick(8)
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
     def color(x, y, color):
         if x < 0 or x >= 120:
-            raise IndexError(f"x out of range: must be >= 0 and < 32, got x = {x}")
+            raise IndexError(f"x out of range: must be >= 0 and < 120, got x = {x}")
         if y < 0 or y >= 67:
-            raise IndexError(f"y out of range: must be >= 0 and < 32, got y = {y}")
+            raise IndexError(f"y out of range: must be >= 0 and < 70, got y = {y}")
         pygame.draw.rect(screen, color, [x * 16, y * 16, 16, 16])
 
     def initi():
         for x in range(120):
-            for y in range(67):
+            for y in range(66):
                 if (x + y) % 2 == 0:
                     color(x, y, (80, 200, 104))
                 else:
                     color(x, y, (3, 150, 40))
 
+        for i in range(120):  
+          a = (i, 31)
+          b = (i, 32)
+          c = (i, 33)
+          path.append(a)
+          path.append(b)
+          path.append(c)
+          color(*a, (128, 128, 128))
+          color(*b, (128, 128, 128))
+          color(*c, (128, 128, 128))
+    def monstre():
+        for i in range(0,len(path),3):
+            color(*path[i], (200,0,0))
+
+
+
+#bon déja g fais un chemin de base parce que blc çççççççççççççç
     initi()
+    
     pygame.display.update()
 
     running = True
@@ -31,4 +55,4 @@ def main():
                 running = False
 
     pygame.quit()
-        
+main()
