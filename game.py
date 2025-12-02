3664da8d485ffb11ea2c2b86d5992d2d5f6ebab2
L0uting <alfredparaire@gmail.com>
2025-12-02 11:27:03 +0100

    Update game.py

--- game.py
+++ game.py
@@ -2,14 +2,9 @@ from random import randint
 import pygame
 from time import sleep
 
-def color(x, y, color):
-    if x < 0 or x >= 32:
-        raise IndexError(f"x out of range: must be >= 0 and < 32, got x = {x}")
-    if y < 0 or y >= 32:
-        raise IndexError(f"y out of range: must be >= 0 and < 32, got y = {y}")
-    pygame.draw.rect(screen, color, [x * 16, y * 16, 16, 16])
+
  
-def init():
+def initi():
           for x in range(32):
             for y in range(32):
               if(x+y)%2==0:color(x,y,(80,200,104))
@@ -17,5 +12,14 @@ def init():
 
 
 pygame.init()
-screen = pygame.display.set_mode((512, 552))
+screen = pygame.display.set_mode((1024, 1024))
+
+def color(x, y, color):
+    if x < 0 or x >= 32:
+        raise IndexError(f"x out of range: must be >= 0 and < 32, got x = {x}")
+    if y < 0 or y >= 32:
+        raise IndexError(f"y out of range: must be >= 0 and < 32, got y = {y}")
+    pygame.draw.rect(screen, color, [x * 16, y * 16, 16, 16])
+
+initi()
     
