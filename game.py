1b59976b04c9d84522c4d2e4b847d68ac48375ba
Luigiday <88960729+luigiday@users.noreply.github.com>
2025-12-02 11:51:57 +0100

    Update game.py

--- game.py
+++ game.py
@@ -2,32 +2,33 @@ from random import randint
 import pygame
 from time import sleep
 
-pygame.init()
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-def color(x, y, color):
-    if x < 0 or x >= 120:
-        raise IndexError(f"x out of range: must be >= 0 and < 32, got x = {x}")
-    if y < 0 or y >= 67:
-        raise IndexError(f"y out of range: must be >= 0 and < 32, got y = {y}")
-    pygame.draw.rect(screen, color, [x * 16, y * 16, 16, 16])
+    def color(x, y, color):
+        if x < 0 or x >= 120:
+            raise IndexError(f"x out of range: must be >= 0 and < 32, got x = {x}")
+        if y < 0 or y >= 67:
+            raise IndexError(f"y out of range: must be >= 0 and < 32, got y = {y}")
+        pygame.draw.rect(screen, color, [x * 16, y * 16, 16, 16])
 
-def initi():
-    for x in range(120):
-        for y in range(67):
-            if (x + y) % 2 == 0:
-                color(x, y, (80, 200, 104))
-            else:
-                color(x, y, (3, 150, 40))
+    def initi():
+        for x in range(120):
+            for y in range(67):
+                if (x + y) % 2 == 0:
+                    color(x, y, (80, 200, 104))
+                else:
+                    color(x, y, (3, 150, 40))
 
-initi()
-pygame.display.update()
+    initi()
+    pygame.display.update()
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-pygame.quit()
-    
+    pygame.quit()
+        
